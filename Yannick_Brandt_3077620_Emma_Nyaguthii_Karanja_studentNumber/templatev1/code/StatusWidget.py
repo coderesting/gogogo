335f325cdbcddaad83e6883df8dd8813cdab36ef
Yannick Brandt <yannick-brandt@outlook.de>
2021-12-03 14:12:35 +0000

    refactor: Change game_status from text to enum

--- Yannick_Brandt_3077620_Emma_Nyaguthii_Karanja_studentNumber/templatev1/code/StatusWidget.py
+++ Yannick_Brandt_3077620_Emma_Nyaguthii_Karanja_studentNumber/templatev1/code/StatusWidget.py
@@ -1,8 +1,11 @@
 from PyQt5.QtWidgets import QWidget
 
+from GameState import GameState
+
 
 class StatusWidget(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
 
-    def set_status(self, status:str): pass
+    def set_status(self, game_state: GameState):
+        print(game_state)
