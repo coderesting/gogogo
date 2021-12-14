94141c9576ac4169dc605dee62f251102b5a26a1
EmmaKaranja <emmmanyaguthii@gmail.com>
2021-12-14 14:11:32 +0000

    temp

--- Yannick_Brandt_3077620_Emma_Nyaguthii_Karanja_studentNumber/templatev1/code/ActionsWidget.py
+++ Yannick_Brandt_3077620_Emma_Nyaguthii_Karanja_studentNumber/templatev1/code/ActionsWidget.py
@@ -1,6 +1,16 @@
-from PyQt5.QtWidgets import QWidget
-
+from PyQt5.QtCore import pyqtSignal
+from PyQt5.QtWidgets import QWidget, QGridLayout, QPushButton
 
 class ActionsWidget(QWidget):
+    reset = pyqtSignal()
+
     def __init__(self, parent=None):
         super().__init__(parent)
+        layout = QGridLayout()
+
+        self.reset_button = QPushButton("Reset", self)
+        self.reset_button.clicked.connect(self.reset.emit)
+        layout.addWidget(self.reset_button, 0, 0)
+
+        self.setLayout(layout)
+
--- Yannick_Brandt_3077620_Emma_Nyaguthii_Karanja_studentNumber/templatev1/code/StatusWidget.py
+++ Yannick_Brandt_3077620_Emma_Nyaguthii_Karanja_studentNumber/templatev1/code/StatusWidget.py
@@ -1,11 +1,10 @@
-from PyQt5.QtWidgets import QWidget
-
+from PyQt5.QtWidgets import QWidget, QGridLayout
 from game.GameState import GameStatus
-
 
 class StatusWidget(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
+        layout = QGridLayout()
 
     def set_status(self, status: GameStatus):
         print(status)
